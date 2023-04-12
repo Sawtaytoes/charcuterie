760fd348ae7014af1d13960dc163598438fa5b47
Giselle Ghadyani <giselleghadyani@gmail.com>
2023-04-11 22:48:05 -0500

    🔧 Updated ESLint config with TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,77 @@ const eslintConfig = {
     '@ghadyani-eslint/node',
     '@ghadyani-eslint/react',
   ],
+  overrides: [
+    {
+      files: '**/*.?(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: [
+        '@typescript-eslint',
+        '@typescript-eslint/eslint-plugin',
+        'deprecation',
+      ],
+      rules: {
+        "no-shadow": "off",
+        "no-use-before-define": "off",
+        '@typescript-eslint/await-thenable': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              Number: {
+                message: 'Use number instead',
+                fixWith: 'number',
+              },
+              String: {
+                message: 'Use string instead',
+                fixWith: 'string',
+              },
+            },
+          },
+        ],
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
+            multiline: {
+              delimiter: 'comma',
+              requireLast: true,
+            },
+            singleline: {
+              delimiter: 'comma',
+              requireLast: false,
+            },
+          },
+        ],
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            format: [
+              'PascalCase'
+            ],
+            selector: 'interface',
+          },
+        ],
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            args: 'after-used',
+            ignoreRestSiblings: false,
+            vars: 'all',
+          },
+        ],
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        '@typescript-eslint/prefer-function-type': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        'deprecation/deprecation': 'warn',
+      },
+    },
+  ],
   parser: '@babel/eslint-parser',
   parserOptions: {
     babelOptions: {
@@ -52,6 +123,16 @@ const eslintConfig = {
       },
     ],
     'indent': 'off', // TEMP. Remove 'off' when fixed in @ghadyani-eslint
+    // 'indent': [
+    //   'error',
+    //   2,
+    //   {
+    //     flatTernaryExpressions: false,
+    //     MemberExpression: 0,
+    //     offsetTernaryExpressions: false,
+    //     SwitchCase: 1,
+    //   },
+    // ],
     'no-unexpected-multiline': 'off',
     'react/react-in-jsx-scope': 'off',
     'react-hooks/exhaustive-deps': 'warn',
