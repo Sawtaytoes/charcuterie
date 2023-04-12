489aee78abd9a30e1307683d32ba46ae442f1d87
Giselle Ghadyani <giselleghadyani@gmail.com>
2023-04-11 21:40:26 -0500

    🎨 Alphabetized keys

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,22 @@
 module.exports = {
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  docs: {
+    autodocs: true,
+  },
   framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
   features: {
-    interactionsDebugger: true
+    interactionsDebugger: true,
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  docs: {
-    autodocs: true
-  }
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
 };
